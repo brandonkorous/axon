64a204ad81a8f271dddbbba7b44ba2aabadf5c2e
Brandon Korous <me@brandonkorous.com>
2026-03-27 18:36:55 -0700

    fix: correct type for applicationServerKey in useNotifications hook

--- frontend/src/hooks/useNotifications.ts
+++ frontend/src/hooks/useNotifications.ts
@@ -62,7 +62,7 @@ export function useNotifications(): UseNotifications {
 
       const sub = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(public_key),
+        applicationServerKey: urlBase64ToUint8Array(public_key).buffer as ArrayBuffer,
       });
 
       // Send subscription to backend
